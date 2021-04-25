a761e014604b0ed1a52b1bbf1a3cd94eac8c2355
junhyeok <zzzskys@naver.com>
2021-04-25 23:15:42 +0900

    10.14까지

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -19,3 +19,6 @@ axios.post<UserType>("/api/auth/signup", body);
 //* 로그인 api
 export const loginAPI = (body: { email: string; password: string}) =>
 axios.post<UserType>("/api/auth/login", body);
+
+//* 쿠키의 access_tocken의 유저 정보 받아오는 api
+export const meAPI = () => axios.get<UserType>("/api/auth/me");
